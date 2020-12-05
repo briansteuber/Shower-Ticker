14b5c4037241993c729224f8cd481538f4ef2641
Brian Steuber <brian.steuber18@gmail.com>
2020-12-05 15:03:29 -0800

    Initial Commit

---
+++ ShowerTicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShowerTicker
+//
+//  Created by Steuber, Brian William on 12/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
